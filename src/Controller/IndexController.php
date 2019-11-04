4bdfa8f7f6cbd9474a27a912eff9e43ce02ff265
Martin <martin.horn@khk.ee>
2019-11-04 13:38:49 +0200

    Using Twig for templating

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -6,10 +6,11 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class IndexController extends AbstractController
 {
+    /**
+     * @Route("/")
+     */
     public function index()
     {
-        return new Response(
-            '<html><body><h1>Booking App index page</h1></body></html>'
-        );
+        return $this->render('main.html.twig');
     }
 }
